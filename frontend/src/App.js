ba7f54ca9b4a6fe3e8a69dd57a3d8cc52a2b6c00
TJSeabury <tylerseabury@gmail.com>
2022-02-21 00:30:37 -0500

    handled images and different node types

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,17 @@ import { uniqueId } from 'lodash';
 function App () {
     const [content, setContent] = useState( null );
 
-    useEffect( () => {
-        async function getContent () {
-            await fetch( 'http://localhost:9000/api/graphql', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify( {
-                    query: `{
+    async function getContent () {
+        await fetch( 'http://localhost:9000/api/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify( {
+                query: `{
                     posts {
+                        featuredImage {
+                            url
+                        }
+                        title
                         publishDate
                         id
                         content {
@@ -21,27 +24,86 @@ function App () {
                         }
                     }
                 }`
-                } ),
-            } )
-                .then( res => res.json() )
-                .then( res => {
-                    setContent( res.data );
-                    console.log( content );
-                } );
+            } ),
+        } )
+            .then( res => res.json() )
+            .then( res => {
+                setContent( res.data );
+                console.log( res.data );
+            } );
+    }
+
+    const renderContentNode = node => {
+        let content = null;
+        switch ( node.type ) {
+            case 'paragraph':
+                content = <p
+                    key={uniqueId( 'paragraph-' )}
+                >
+                    {node.children[0].text}
+                </p>;
+                break;
+            case 'heading':
+                switch ( node.level ) {
+                    case 1:
+                    case 2:
+                        content = <h2
+                            key={uniqueId( 'heading-' )}
+                        >
+                            {node.children[0].text}
+                        </h2>;
+                        break;
+                    case 3:
+                        content = <h3
+                            key={uniqueId( 'heading-' )}
+                        >
+                            {node.children[0].text}
+                        </h3>;
+                        break;
+                    case 4:
+                    case 5:
+                    case 6:
+                        content = <h4
+                            key={uniqueId( 'heading-' )}
+                        >
+                            {node.children[0].text}
+                        </h4>;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 'code':
+                content = <pre
+                    key={uniqueId( 'code-' )}
+                ><code>
+                        {node.children[0].text}
+                    </code></pre>;
+                break;
+            default:
+                break;
         }
+        return content;
+    };
+
+    useEffect( () => {
         getContent();
-        return () => {
-        };
-    }, [content] );
+        return () => { };
+    }, [] );
 
     const RenderContent = ( props ) => {
+        if ( !props.content ) return <div>Nothing to show yet.</div>;
         const post = props.content?.posts[0];
-        if ( !post ) return <div>Nothing to show yet.</div>;
-        console.log( post );
         return <article
             id={post.id}
         >
-            {post.content.document.map( p => <p key={uniqueId( 'para-' )}>{p.children[0].text}</p> )}
+            <header>
+                <figure>
+                    <img src={`http://localhost:9000/${post.featuredImage.url}`} alt='' />
+                </figure>
+                <h1>{post.title}</h1>
+            </header>
+            {post.content.document.map( node => renderContentNode( node ) )}
         </article>;
     };
 
