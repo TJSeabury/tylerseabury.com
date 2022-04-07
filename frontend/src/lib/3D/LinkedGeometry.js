79e5506b0edeb49c139b1682ff78b851c01d0c0b
TJSeabury <tylerseabury@gmail.com>
2022-04-07 00:00:41 -0400

    working on improving methods

--- frontend/src/lib/3D/LinkedGeometry.js
+++ frontend/src/lib/3D/LinkedGeometry.js
@@ -3,56 +3,95 @@ class LinkedPoint {
     constructor( sourceArray, vector, indexes ) {
         this.array = sourceArray;
         this.vector = vector;
-        this.indexes = [...indexes];
+        this.indexes = indexes;
     }
-    updateGeometry() {
+    updateGeometry () {
         for ( const i of this.indexes ) {
             this.array[i] = this.vector.x;
-            this.array[i+1] = this.vector.y;
-            this.array[i+2] = this.vector.z;
+            this.array[i + 1] = this.vector.y;
+            this.array[i + 2] = this.vector.z;
         }
     }
-    setX( x ){
+    setX ( x ) {
         this.vector.x = x;
         this.updateGeometry();
     }
-    setY( y ){
+    setY ( y ) {
         this.vector.y = y;
         this.updateGeometry();
     }
-    setZ( z ){
+    setZ ( z ) {
         this.vector.z = z;
         this.updateGeometry();
     }
-    setVector( vector ){
+    setVector ( vector ) {
         this.vector = vector;
         this.updateGeometry();
     }
 }
+
 export class LinkedGeometry {
     constructor( geometry ) {
         this.geometry = geometry;
-        const pos = geometry.attributes.position.array;
-        this.points = [];
-        for ( let i = 0; i < pos.length; i += 3 ) {
-            let v = new THREE.Vector3( Math.fround(pos[i]), Math.fround(pos[i+1]), Math.fround(pos[i+2]) );
-            let pi = -1;
-            this.points.forEach( (p, idx) => {
-                if (  v.equals( p.vector ) ) {
-                    pi = idx;
-                }
-            } );
-            if ( pi >= 0 ) {
-                this.points[pi].indexes.push(i);
-            }
-            else {
-                this.points.push( new LinkedPoint( pos, v, [i] ) );
-            }
+        this.array = geometry.attributes.position.array;
+        this.index = {};
+
+        let minX = Infinity;
+        let maxX = -Infinity;
+        let minZ = Infinity;
+        let maxZ = -Infinity;
+
+        for ( let i = 0; i < this.array.length; i += 3 ) {
+
+            minX = Math.min( minX, this.array[i] );
+            maxX = Math.max( maxX, this.array[i] );
+            minZ = Math.min( minZ, this.array[i + 2] );
+            maxZ = Math.max( maxZ, this.array[i + 2] );
+
+            this.index[[
+                this.array[i],
+                0,
+                this.array[i + 2]
+            ]] = i;
         }
+        console.log( 'Extremes: ', minX, maxX, minZ, maxZ, 'Size: ', this.array.length, this.array );
+        console.log( this.index, Object.values( this.index ).length );
     }
-    forEach( f ){
-        for ( const p of this.points ) {
-            f( p );
+
+    getPointIndex ( x, z ) {
+        console.log( [
+            x,
+            0,
+            z
+        ].toString() );
+        return this.index[[
+            x,
+            0,
+            z
+        ]];
+    }
+
+    getPoint ( x, z ) {
+        console.log( 'getPoint: ', x, z );
+        const p = this.getPointIndex( x, z );
+        return {
+            x: this.array[p.x],
+            y: this.array[p.y],
+            z: this.array[p.z]
+        };
+    }
+
+    forEach ( f ) {
+        const pos = this.geometry.attributes.position.array;
+        for ( let i = 0; i < pos.length; i += 3 ) {
+            let v = new THREE.Vector3(
+                Math.fround( pos[i] ),
+                Math.fround( pos[i + 1] ),
+                Math.fround( pos[i + 2] ) );
+            v = f( v );
+            pos[i] = v.x;
+            pos[i + 1] = v.y;
+            pos[i + 2] = v.z;
         }
     }
 }
