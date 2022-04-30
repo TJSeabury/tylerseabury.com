0d3108518fdb67bde363f0ef41c8a2d0fac18c1f
TJSeabury <tylerseabury@gmail.com>
2022-04-30 15:49:19 -0400

    config changes

--- dashboard/keystone.ts
+++ dashboard/keystone.ts
@@ -15,32 +15,32 @@ import { lists } from './schema';
 import { withAuth, session } from './auth';
 
 export default withAuth(
-    // Using the config function helps typescript guide you to the available options.
-    config({
-        server: {
-            cors: {
-                origin: '*'
-            },
-            port: 9000
-        },
-        // the db sets the database provider - we're using sqlite for the fastest startup experience
-        db: {
-            provider: 'sqlite',
-            url: 'file:./keystone.db',
-        },
-        // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
-        ui: {
-            // For our starter, we check that someone has session data before letting them see the Admin UI.
-            isAccessAllowed: (context) => !!context.session?.data,
-        },
-        lists,
-        session,
-        images: {
-            upload: 'local',
-            local: {
-                storagePath: 'public/images',
-                baseUrl: '/images',
-            },
-        }
-    })
+  // Using the config function helps typescript guide you to the available options.
+  config({
+    server: {
+      cors: {
+        origin: '*'
+      },
+      port: 9001
+    },
+    // the db sets the database provider - we're using sqlite for the fastest startup experience
+    db: {
+      provider: 'sqlite',
+      url: 'file:./keystone.db',
+    },
+    // This config allows us to set up features of the Admin UI https://keystonejs.com/docs/apis/config#ui
+    ui: {
+      // For our starter, we check that someone has session data before letting them see the Admin UI.
+      isAccessAllowed: (context) => !!context.session?.data,
+    },
+    lists,
+    session,
+    images: {
+      upload: 'local',
+      local: {
+        storagePath: 'public/images',
+        baseUrl: '/images',
+      },
+    }
+  })
 );
